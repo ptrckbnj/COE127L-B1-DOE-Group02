fbbe795e491e62a51e68b2ace0f8cd90a85be075
gioasm <45848726+gioasm@users.noreply.github.com>
2019-07-26 16:12:09 +0800

    Add files via upload

---
+++ inheritancecvclass8simplekernel.cpp
@@ -0,0 +1,54 @@
+cv::Mat GaussianBlur_omp(cv::Mat image, int num_procs) {
+    cv::Mat returnImage = image;
+
+
+    double sigma = 1;
+    int W = 5;
+    double kernel[W][W];
+    double mean = W/2;
+    double sum = 0.0; 
+    for (int x = 0; x < W; ++x)
+        for (int y = 0; y < W; ++y) {
+            kernel[x][y] = exp( -0.5 * (pow((x-mean)/sigma, 2.0) + pow((y-mean)/sigma,2.0)) )
+                / (2 * M_PI * sigma * sigma);
+            sum += kernel[x][y];
+        }
+
+    for (int x = 0; x < W; ++x)
+        for (int y = 0; y < W; ++y)
+            kernel[x][y] /= sum;
+
+
+
+    omp_set_num_threads(num_procs);
+    int i, j;
+    int radius = W;
+    double val1 = 0, val2 = 0, val3 = 0;
+
+#pragma omp parallel for shared(image) private(i,j,val1,val2,val3) 
+    for (i = 0; i < image.cols; i++) {
+        for (j = 0; j < image.rows; j++) {
+
+            int iy;
+            int ix;
+
+            for (iy = -radius/2; iy <= radius/2; iy++) {
+                for (ix = -radius/2; ix <= radius/2; ix++) {
+                    int x = std::min(image.cols, std::max(0, i-ix));
+                    int y = std::min(image.rows, std::max(0, j-iy));
+
+                    val1 += (image.at<cv::Vec3b>(y, x)[0] * kernel[iy+2][ix+2]);
+                    val2 += (image.at<cv::Vec3b>(y, x)[1] * kernel[iy+2][ix+2]);
+                    val3 += (image.at<cv::Vec3b>(y, x)[2] * kernel[iy+2][ix+2]);
+                } 
+            }
+
+            returnImage.at<cv::Vec3b>(j, i)[0] = int(val1);
+            returnImage.at<cv::Vec3b>(j, i)[1] = int(val2);
+            returnImage.at<cv::Vec3b>(j, i)[2] = int(val3);
+            val1 = val2 = val3 = 0;
+        }
+    }
+    return returnImage;
+}
+
