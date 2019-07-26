846a81103b126f0383e97904a332878954291563
Patrick Banjao <50915438+ptrckbnj@users.noreply.github.com>
2019-07-26 18:08:03 +0800

    Update cvclasswithoutOpenMp.cpp

--- cvclasswithoutOpenMp.cpp
+++ cvclasswithoutOpenMp.cpp
@@ -3,7 +3,15 @@ cv::Mat GaussianBlur(cv::Mat image) {
 
     double sigma = 1;
     int W = 5;
-
+    double mean = W/2;
+    double kernel[W][W];
+    double sum = 0.0;
+    for (int x = 0; x < W; ++x)
+        for (int y = 0; y < W; ++y) {
+            kernel[x][y] = exp( -0.5 * (pow((x-mean)/sigma, 2.0) + pow((y-mean)/sigma,2.0)) )
+                / (2 * M_PI * sigma * sigma);
+            sum += kernel[x][y];
+        }
 
     for (int x = 0; x < W; ++x)
         for (int y = 0; y < W; ++y)
@@ -18,7 +26,6 @@ cv::Mat GaussianBlur(cv::Mat image) {
             int ix;
             for (ix = -radius/2 ; ix <= radius/2; ix++) {
                 for (iy = -radius/2; iy <= radius/2; iy++) {
-
                     int x = std::min(image.cols, std::max(0, i-ix));
                     int y = std::min(image.rows, std::max(0, j-iy));
                     val1 += (image.at<cv::Vec3b>(y, x)[0] * kernel[iy+2][ix+2]);
@@ -27,9 +34,11 @@ cv::Mat GaussianBlur(cv::Mat image) {
                 } 
             }
 
+            returnImage.at<cv::Vec3b>(j, i)[0] = int(val1);
+            returnImage.at<cv::Vec3b>(j, i)[1] = int(val2);
+            returnImage.at<cv::Vec3b>(j, i)[2] = int(val3);
+            val1 = val2 = val3 = 0;
         }
     }
     return returnImage;
 }
-
-
