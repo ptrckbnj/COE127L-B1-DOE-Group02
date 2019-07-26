f0e789cf31f50bd71895d1203a6ef327741f2c57
MiecaelaGarcia <52278032+MiecaelaGarcia@users.noreply.github.com>
2019-07-27 03:17:38 +0800

    Update inheritance.cpp

--- inheritance.cpp
+++ inheritance.cpp
@@ -20,3 +20,13 @@ v::Mat GaussianBlur_omp_2(cv::Mat image, int num_procs) {
             kernel[x][y] /= sum;
 
     omp_set_num_threads(num_procs);
+
+    int i, j;
+    int radius = W;
+    double val1 = 0, val2 = 0, val3 = 0;
+#pragma omp parallel for shared(image) private(i) 
+    for (i = 0; i < image.cols; i++) {
+#pragma omp parallel for shared(image) private(j,val1,val2,val3) 
+        for (j = 0; j < image.rows; j++) {
+            int iy;
+            int ix;
