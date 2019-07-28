97df5f027b0fca24ecc0053fafdd263743597934
MiecaelaGarcia <52278032+MiecaelaGarcia@users.noreply.github.com>
2019-07-28 11:28:40 +0800

    Update and rename inheritance.cpp to inheritancecvclass8doublekernel.cpp

--- inheritancecvclass8doublekernel.cpp
+++ inheritancecvclass8doublekernel.cpp
@@ -21,6 +21,8 @@ v::Mat GaussianBlur_omp_2(cv::Mat image, int num_procs) {
 
     omp_set_num_threads(num_procs);
 
+    
+//July 27, 2019
     int i, j;
     int radius = W;
     double val1 = 0, val2 = 0, val3 = 0;
@@ -30,3 +32,26 @@ v::Mat GaussianBlur_omp_2(cv::Mat image, int num_procs) {
         for (j = 0; j < image.rows; j++) {
             int iy;
             int ix;
+
+            
+//July 28, 2019
+            for (iy = -radius/2; iy <= radius/2; iy++) {
+                for (ix = -radius/2; ix <= radius/2; ix++) {
+                    int x = std::min(image.cols, std::max(0, i-ix));
+                    int y = std::min(image.rows, std::max(0, j-iy));
+
+                    val1 += (image.at<cv::Vec3b>(y, x)[0] * kernel[iy+2][ix+2]);
+                    val2 += (image.at<cv::Vec3b>(y, x)[1] * kernel[iy+2][ix+2]);
+                    val3 += (image.at<cv::Vec3b>(y, x)[2] * kernel[iy+2][ix+2]);
+                } 
+            }
+
+            returnImage.at<cv::Vec3b>(j, i)[0] = int(val1);
+            returnImage.at<cv::Vec3b>(j, i)[1] = int(val2);
+            returnImage.at<cv::Vec3b>(j, i)[2] = int(val3);
+            val1 = val2 = val3 = 0;
+        }
+    }
+    return returnImage;
+}
+
